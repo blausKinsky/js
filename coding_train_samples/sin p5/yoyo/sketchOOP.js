47ed53502f1dc3fb46356c9f32d7405fb78c9bae
blausKinsky <loopingthought@gmail.com>
2021-07-06 15:28:15 -0400

    blah

--- coding_train_samples/sin p5/yoyo/sketchOOP.js
+++ coding_train_samples/sin p5/yoyo/sketchOOP.js
@@ -9,25 +9,30 @@ let y = 0;
 function setup() {
   createCanvas(600, 400);
   background(0, 127);
-  pen = new Pendulum(angle, 'musk');
+  pen = new Pendulum(angle);
   // frameRate(60);
 }
 function draw() {
   translate(300, 200);// could this go in the setup? 
-  angle += angleV;
-  aCycle = angle%TWO_PI; 
-  console.log(angle, aCycle);
+  // angle += angleV;
+  // aCycle = angle%TWO_PI; 
+  // console.log(angle, aCycle);
+  
+  console.log('draw ', angle, angleV);
+  // aCycle = angle%TWO_PI; 
+  pen.display(angle);
   yWord = textArray[1];
-  y = map(sin(angle), -1, 1, -200, 200);
-  pen.display(y, yWord);
   angle += angleV;
 }
 
 class Pendulum{
-  constructor(y, word){
-    this.y = y;
+  constructor(angle){
+    // this.y = y;
     // this.angleV = angleV;
-    this.word = word; 
+    // this.word = word; 
+    this.angle = angle; 
+    console.log('in the constructor', this.angle);
+    
   }
   // move(){
   //   this.y = map(sin(this.angle), -1, 1, -200, 200); 
@@ -35,10 +40,13 @@ class Pendulum{
   // }
   display(){
     // this.y = map(sin(this.angle), -1, 1, -200, 200); 
-    fill(int(random(255)), int(random(255)), int(random(255)));
+    let y = map(sin(this.angle), -1, 1, -200, 200);
+    console.log('y is ', y);
+    fill(238, 38, 38); 
+    // fill(int(random(255)), int(random(255)), int(random(255)));
     noStroke();
     textSize(34);
-    text(this.word, 0, this.y);
+    text('maintenance', 0, y);
   }
 }
 
