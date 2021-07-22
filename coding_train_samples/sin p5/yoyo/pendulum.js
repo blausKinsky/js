1a5be7aebe5a88f8e0388b3c66f29a8ac99002f7
blausKinsky <loopingthought@gmail.com>
2021-07-22 14:18:15 -0400

    updated the yoyo, class version, i done good me thinks

--- coding_train_samples/sin p5/yoyo/pendulum.js
+++ coding_train_samples/sin p5/yoyo/pendulum.js
@@ -1,37 +1,12 @@
 class Pendulum{
-  constructor(angle, angleV,myX,myY,typ){
+  constructor(angle, angleV,myX,myY,typ, myClr,myText){
     this.angle = angle;
     this.angleV = angleV;
     this.x = myX;
     this.y = myY; 
     this.typ = typ; 
-    // console.log('in the constructor', this.angle, this.angleV);
-
-  }
-  display(myClr, myText){
-    // print(counter);
-    this.myClr = myClr;
-    this.myText = myText;
-    this.angle += this.angleV;
-    aCycle = this.angle%TWO_PI;
-    // console.log(this.angle, aCycle);
-    let x = map(sin(this.angle), -1, 1, -300, 300);
-    let y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-    // fill(this.myClr);
-    // noStroke();
-    // textSize(34);
-    // text(this.myText, 0, y);
-    //this is where I tried to put the changing text when the cycle hits 0
-    if(aCycle < (this.angleV + offset) && aCycle > 0.0){
-      this.myText = this.chooseWord();
-      let newWord = this.chooseWord();
-      // console.log('aCycle HIT in counter 0', this.myText, newWord);
-    }
-    console.log('this color: ', this.myClr, aCycle);
-    fill(this.myClr);
-    noStroke();
-    textSize(34);
-    text(this.myText, x, y);
+    this.clr = myClr;
+    this.txt = myText; 
   }
 
   typeSetter(typ, angleV,angle){
@@ -39,130 +14,105 @@ class Pendulum{
       switch(typ){
         case 0:
           aCycle = angle%TWO_PI; 
-          // x = map(sin(this.angle), -1, 1, -300, 300);
-          // y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-          console.log('the type is 0', typ, angleV, angle, aCycle);
+          this.x = map(sin(angle), -1, 1, -300, 300);
+          this.y = map(sin(angle), -1, 1, (-1*height/2), height/2);
+          // console.log('the type is 0', typ, angleV, angle, aCycle, 'the x and y are ', this.x,this.y);
 
           if(aCycle < (angleV + offset) && aCycle > 0.0){
-            // tempText = chooseWord();
-            // tempColor = chooseColor(); 
-            console.log('aCycle HIT in counter 0');
+            this.txt = textArray[int(random(textArray.length))]; 
+            this.clr = myColor[int(random(myColor.length))]; 
+            // console.log('aCycle HIT in case 0', this.txt, this.clr);
+          // console.log('aCycle HIT in counter 0', textArray[int(random(textArray.length))], myColor[int(random(myColor.length))]);
           }
           //question about whether I should be using this.typ, this.angleV, this.angle
 
         break;
 
         case 1:
-          console.log('the type is 1', typ, angleV, angle);
+          a2Cycle = angle%TWO_PI; 
+          this.x = map(sin(angle), -1, 1, -300, 300);
+          this.y = map(sin(angle), -1, 1, (-1*height/2), height/2);
+          // console.log('the type is 0', typ, angleV, angle, a2Cycle, 'the x and y are ', this.x,this.y);
+
+          if(a2Cycle < (angleV + offset) && a2Cycle > 0.0){
+            this.txt = textArray[int(random(textArray.length))]; 
+            this.clr = myColor[int(random(myColor.length))]; 
+            // console.log('a2Cycle HIT in case 1', this.txt, this.clr);
+   
+          // console.log('a2Cycle HIT in counter 0', textArray[int(random(textArray.length))], myColor[int(random(myColor.length))]);
+          }
+          // console.log('the type is 1', typ, angleV, angle);
 
         break;
 
         case 2:
-          console.log('the type is 2', typ, angleV, angle);
+          a3Cycle = angle%TWO_PI; 
+          this.x = map(sin(angle), -1, 1, -300, 300);
+          this.y = map(sin(angle), -1, 1, (-1*height/2), height/2);
+          // console.log('the type is 0', typ, angleV, angle, a3Cycle, 'the x and y are ', this.x,this.y);
+
+          if(a3Cycle < (angleV + offset) && a3Cycle > 0.0){
+            this.txt = textArray[int(random(textArray.length))]; 
+            this.clr = myColor[int(random(myColor.length))]; 
+            // console.log('aC3ycle HIT in case 2', this.txt, this.clr);
+   
+          // console.log('a3Cycle HIT in counter 0', textArray[int(random(textArray.length))], myColor[int(random(myColor.length))]);
+          }
+          // console.log('the type is 2', typ, angleV, angle);
 
         break;
 
         case 3:
-          console.log('the type is 3', typ, angleV, angle);   
+          a4Cycle = angle%TWO_PI; 
+          this.x = map(sin(angle), -1, 1, -300, 300);
+          this.y = map(sin(angle), -1, 1, (-1*height/2), height/2);
+          // console.log('the type is 0', typ, angleV, angle, a4Cycle, 'the x and y are ', this.x,this.y);
+
+          if(a4Cycle < (angleV + offset) && a4Cycle > 0.0){
+            this.txt = textArray[int(random(textArray.length))]; 
+            this.clr = myColor[int(random(myColor.length))]; 
+            // console.log('a4Cycle HIT in case 3', this.txt, this.clr);
+   
+          // console.log('a4Cycle HIT in counter 0', textArray[int(random(textArray.length))], myColor[int(random(myColor.length))]);
+          }
+          // console.log('the type is 3', typ, angleV, angle);   
 
         break;
 
         case 4:
-          console.log('the type is 4', typ, angleV, angle);
+          // console.log('the type is 4', typ, angleV, angle);
+          a5Cycle = angle%TWO_PI; 
+          this.x = map(sin(angle), -1, 1, -300, 300);
+          this.y = map(sin(angle), -1, 1, (-1*height/2), height/2);
+          // console.log('the type is 0', typ, angleV, angle, a5Cycle, 'the x and y are ', this.x,this.y);
+
+          if(a5Cycle < (angleV + offset) && a5Cycle > 0.0){
+            this.txt = textArray[int(random(textArray.length))]; 
+            this.clr = myColor[int(random(myColor.length))]; 
+            // console.log('a5Cycle HIT in case 4 ', this.txt, this.clr);
+   
+          // console.log('a5Cycle HIT in case 4 ', textArray[int(random(textArray.length))], myColor[int(random(myColor.length))]);
+          }
 
         break;
 
         case 5:
-          console.log('the type is 5', typ, angleV, angle);
+          a6Cycle = angle%TWO_PI; 
+          this.x = map(sin(angle), -1, 1, -300, 300);
+          this.y = map(sin(angle), -1, 1, (-1*height/2), height/2);
+          // console.log('the type is 0', typ, angleV, angle, a6Cycle, 'the x and y are ', this.x,this.y);
+
+          if(a6Cycle < (angleV + offset) && a6Cycle > 0.0){
+            this.txt = textArray[int(random(textArray.length))]; 
+            this.clr = myColor[int(random(myColor.length))]; 
+            // console.log('a6Cycle HIT in case 5', this.txt, this.clr);
+            // console.log('a6Cycle HIT in counter 0', textArray[int(random(textArray.length))], myColor[int(random(myColor.length))]);
+          }
+          // console.log('the type is 5', typ, angleV, angle);
 
         break;
       }
-
-
   }
-    
-    // if (counter == 1){
-    //       this.angle += this.angleV;
-    //       // print('counter 1', this.angle, this.angleV);
-    //       let x = map(sin(this.angle), -1, 1,(-1*width/2), width/2);
-    //       // let y = map(sin(this.angle), -1, 1, -200, 200);
-    //       fill(this.myClr);
-    //       noStroke();
-    //       textSize(34);
-          // text(this.myText, x, 0);
-    //     }
-    // if (counter == 2){
-    //       this.angle += this.angleV;
-    //       // print('counter 2', this.angle, this.angleV);
-    //       let x = map(sin(this.angle), -1, 1, (-1*width/2), width/2);
-    //       let y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-    //       fill(this.myClr);
-    //       noStroke();
-    //       textSize(34);
-    //       text(this.myText, x, y);
-    //     }
-
-    // if (counter == 3){
-    //       this.angle += this.angleV;
-    //       // let newAngle = this.angleV + this.angleV;
-    //       // this.angle += newAngle;
-    //       // print('counter 3', 'newAngle is ', this.angle, this.angleV);
-    //       let x = map(cos(this.angle), -1, 1, (-1*width/2), width/2);
-    //       let y = map(cos(this.angle), -1, 1, (-1*height/2), height/2);
-    //       fill(this.myClr);
-    //       noStroke();
-    //       textSize(34);
-          // text(this.myText, x, y);
-    //     }
-    // if (counter == 4){
-    //       // let newXangle = this.angleV + this.angleV;
-    //       // let newYangle = this.angle;
-    //       // this.angle += newXangle;
-    //       // newYangle += this.angleV;
-    //       this.angle += this.angleV;
-    //       // print('counter 4', 'newAngle is ', newXangle, this.angle, this.angleV);
-    //       let x = map(cos(this.angle), -1, 1, (-1*width/2), width/2);
-    //       let y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-    //       fill(this.myClr);
-    //       noStroke();
-    //       textSize(34);
-    //       text(this.myText, x, y);
-    // }
-
-    // if (counter == 5) {
-    //       this.angle += this.angleV;
-    //       // let newYangle2 = this.angleV + this.angleV;
-    //       // print('counter is ', counter, this.angle, this.angleV);
-    //       let x = map(sin(this.angle), -1, 1, (-1*width/2), width/2);
-    //       let y = map(cos(this.angle), -1, 1, (-1*height/2), height/2);
-    //       fill(this.myClr);
-    //       noStroke();
-    //       textSize(34);
-    //       text(this.myText, x, y);
-    // }
-
-
-    // counter += 1;
-    // if (counter>myColor.length-1){
-    //   counter = 0;
-    // }
-// console.log('this is the counter', counter); 
-
-    chooseColor(){
-      let ranColor = int(random(myColor.length));
-      // print(ranColor, myColor[ranColor]);
-      return myColor[ranColor];
-    }
-    chooseWord(){
-      let ranWord = int(random(textArray.length));
-      // print(ranWord, textArray[ranWord]);
-      return textArray[ranWord];
-    }
-
 }
+    
 
-
-// //     randomChoice(arr) {
-// //     return arr[Math.floor(Math.random() * arr.length)];
-// // }
-// }
--- coding_train_samples/sin p5/yoyo/sketch.js
+++ coding_train_samples/sin p5/yoyo/sketch.js
@@ -3,7 +3,8 @@ let [aCycle, a2Cycle, a3Cycle, a4Cycle, a5Cycle, a6Cycle] = [0,0,0,0,0,0];
 let offset = 0.000005;
 let textArray = ['musk', 'maneuver', 'makeup', 'manhandle','sogged', 'soaked', 'sprayed', 'slipped','backlit', 'frontlit', 'twinkle', 'sidelight'];
 let myColor = ['red', 'blue','orange','yellow', 'green','magenta'];
-// let y = 0;
+let y = 0;
+let x = 0; 
 let aV = 0;
 let angleV = [];
 let pen = [];
@@ -16,14 +17,12 @@ let inc = 0.0075;
 let aVmin = 0.01275;
 let vStart = 0.200
 let vectorSetup = [];
-
-
-
+let tempText = ''; 
+let tempColor = myColor[1]; 
 // let colorArray = [(238, 238, 33),(33, 238, 238),(238, 33, 238),(33, 238, 33),(238, 33, 33),(33, 33, 238)];
 
 function setup() {
   createCanvas(600, 400);
-  
   for(let i=0; i<myColor.length; i++ ){
     // aV = aVmin + inc;
     // aV += aV
@@ -31,145 +30,29 @@ function setup() {
     // vectorSetup[i] = aV; 
     angleV = append(angleV, aV);// I'm using a for loop to generate the angleV array
     print('i is ', i, aV, angleV);
-    pen[i] = new Pendulum(angle, angleV[i], 0, 0, i);
-    // myClr[i] = pen[i].chooseColor();
-    // myText[i] = pen[i].chooseWord();
-    // print(myClr[i], myText[i]);
+
+    myClr[i] = myColor[int(random(myColor.length))];
+    myText[i] = textArray[int(random(textArray.length))];
+    pen[i] = new Pendulum(angle, angleV[i], 0, 0, i, myClr[i],myText[i]);
   }
 }
 
 function draw() {
-  background(0, 53);
+  background(50, 200);
   translate(300, 200);
   // aCycle = angle%TWO_PI;
   for (let i = 0; i<myColor.length; i++){
-      
       angle[i] += angleV[i]; 
       pen[i].typeSetter(i,angleV[i], angle[i]); 
-      // pen[i].display(myClr[i], myText[i]);
+      // console.log('pen ', i, 'ANGLE is ', pen[i].angle); 
+      // console.log('the x & y parameter ', pen[i].x, pen[i].y);
+      // console.log('THE TEXT AND COLOR ARE ', pen[i].txt, pen[i].clr);
+      fill(pen[i].clr);
+      noStroke();
+      textSize(34);
+      text(pen[i].txt, pen[i].x, pen[i].y);
   }
 }
-
-// class Pendulum{
-//   constructor(angle, angleV){
-//     this.angle = angle;
-//     this.angleV = angleV;
-//     //why don't i need to declare the data color in the constructor? (although it does work with it)
-//     // You can declare a class attribute in any class function - it just makes more sense to do it here in the constructor so it only happens once, rather than doing it every time we call the display function.
-//     // console.log('in the constructor', this.angle, this.angleV);
-
-//   }
-//   display(myClr, myText){
-//     // print(counter);
-//     this.myClr = myClr;
-//     this.myText = myText;
-
-
-//     if (counter == 0){
-//           this.angle += this.angleV;
-//           aCycle = this.angle%TWO_PI;
-//           console.log(this.angle, aCycle);
-//           // let x = map(sin(this.angle), -1, 1, -300, 300);
-//           let y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-//           // fill(this.myClr);
-//           // noStroke();
-//           // textSize(34);
-//           // text(this.myText, 0, y);
-//           //this is where I tried to put the changing text when the cycle hits 0
-//       //     if(aCycle < (this.angleV + offset) && aCycle > 0.0){
-//       //       this.myText = this.chooseWord();
-//       //       let newWord = this.chooseWord();
-//       //       console.log('aCycle HIT in counter 0', this.myText, newWord);
-//       // }
-//           console.log('this color: ', this.myClr)
-//           fill(this.myClr);
-//           noStroke();
-//           textSize(34);
-//           text(this.myText, 0, y);
-//     }
-//     if (counter == 1){
-//           this.angle += this.angleV;
-//           // print('counter 1', this.angle, this.angleV);
-//           let x = map(sin(this.angle), -1, 1,(-1*width/2), width/2);
-//           // let y = map(sin(this.angle), -1, 1, -200, 200);
-//           fill(this.myClr);
-//           noStroke();
-//           textSize(34);
-//           text(this.myText, x, 0);
-//         }
-//     if (counter == 2){
-//           this.angle += this.angleV;
-//           // print('counter 2', this.angle, this.angleV);
-//           let x = map(sin(this.angle), -1, 1, (-1*width/2), width/2);
-//           let y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-//           fill(this.myClr);
-//           noStroke();
-//           textSize(34);
-//           text(this.myText, x, y);
-//         }
-
-//     if (counter == 3){
-//           this.angle += this.angleV;
-//           // let newAngle = this.angleV + this.angleV;
-//           // this.angle += newAngle;
-//           // print('counter 3', 'newAngle is ', this.angle, this.angleV);
-//           let x = map(cos(this.angle), -1, 1, (-1*width/2), width/2);
-//           let y = map(cos(this.angle), -1, 1, (-1*height/2), height/2);
-//           fill(this.myClr);
-//           noStroke();
-//           textSize(34);
-//           text(this.myText, x, y);
-//         }
-//     if (counter == 4){
-//           // let newXangle = this.angleV + this.angleV;
-//           // let newYangle = this.angle;
-//           // this.angle += newXangle;
-//           // newYangle += this.angleV;
-//           this.angle += this.angleV;
-//           // print('counter 4', 'newAngle is ', newXangle, this.angle, this.angleV);
-//           let x = map(cos(this.angle), -1, 1, (-1*width/2), width/2);
-//           let y = map(sin(this.angle), -1, 1, (-1*height/2), height/2);
-//           fill(this.myClr);
-//           noStroke();
-//           textSize(34);
-//           text(this.myText, x, y);
-//     }
-
-//     if (counter == 5) {
-//           this.angle += this.angleV;
-//           // let newYangle2 = this.angleV + this.angleV;
-//           // print('counter is ', counter, this.angle, this.angleV);
-//           let x = map(sin(this.angle), -1, 1, (-1*width/2), width/2);
-//           let y = map(cos(this.angle), -1, 1, (-1*height/2), height/2);
-//           fill(this.myClr);
-//           noStroke();
-//           textSize(34);
-//           text(this.myText, x, y);
-//     }
-
-
-//     counter += 1;
-//     if (counter>myColor.length-1){
-//       counter = 0;
-//     }
-
-//   }
-
-//     chooseColor(){
-//       let ranColor = int(random(myColor.length));
-//       // print(ranColor, myColor[ranColor]);
-//       return myColor[ranColor];
-//     }
-//     chooseWord(){
-//       let ranWord = int(random(textArray.length));
-//       // print(ranWord, textArray[ranWord]);
-//       return textArray[ranWord];
-//     }
-
-// //     randomChoice(arr) {
-// //     return arr[Math.floor(Math.random() * arr.length)];
-// // }
-// }
 
 
 
